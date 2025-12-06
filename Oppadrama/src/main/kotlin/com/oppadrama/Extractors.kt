eeb579ba49ffa903a5678cfb2e07c3bd329acbb5
Asm0d3usX <117481415+Asm0d3usX@users.noreply.github.com>
2025-12-06 08:08:52 +0800

    Update Extractors.kt

--- Oppadrama/src/main/kotlin/com/oppadrama/Extractors.kt
+++ Oppadrama/src/main/kotlin/com/oppadrama/Extractors.kt
@@ -2,17 +2,15 @@ package com.oppadrama
 
 import com.lagradost.api.Log
 import com.lagradost.cloudstream3.app
-import com.lagradost.cloudstream3.extractors.Filesim
+import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
 import com.lagradost.cloudstream3.extractors.VidHidePro
-
-
 
 class Smoothpre: VidHidePro() {
     override var name = "EarnVids"
     override var mainUrl = "https://smoothpre.com"
 }
 
-class Turbovidhls : Filesim() {
-    override val name = "Turbovidhls"
+class Emturbovid : EmturbovidExtractor() {
+    override var name = "Emturbovid"
     override var mainUrl = "https://emturbovid.com"
 }
