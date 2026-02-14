99b10d7ff241d40c1edf67ce4afaf8c136b256a0
duro92 <pendisu647@gmail.com>
2026-02-14 08:49:03 +0700

    Fix

--- Gojodesu/src/main/kotlin/com/gojodesu/Extractors.kt
+++ Gojodesu/src/main/kotlin/com/gojodesu/Extractors.kt
@@ -10,7 +10,6 @@ import com.lagradost.cloudstream3.utils.newExtractorLink
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.extractors.Filesim
-import com.lagradost.cloudstream3.extractors.EmturbovidExtractor
 import com.lagradost.cloudstream3.utils.loadExtractor
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
 import org.jsoup.Jsoup
@@ -50,7 +49,61 @@ open class Kotakajaib : ExtractorApi() {
     }
 }
 
-class Emturbovid : EmturbovidExtractor() {
+open class EmturbovidExtractor : ExtractorApi() {
     override var name = "Emturbovid"
-    override var mainUrl = "https://turbovidhls.com"
+    override var mainUrl = "https://emturbovid.com"
+    override val requiresReferer = false
+
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
+    val links = mutableListOf<ExtractorLink>()
+    val firstReferer = referer ?: "$mainUrl/"
+
+    // Step 1: resolve redirect chain
+    val resp = app.get(url, referer = firstReferer, allowRedirects = true)
+    var finalUrl = resp.url
+
+    // Step 2: kalau response adalah m3u8 langsung
+    val contentType = resp.headers["content-type"] ?: ""
+    if (contentType.contains("mpegurl") || finalUrl.contains(".m3u8")) {
+        links.add(
+            newExtractorLink(
+                source = name,
+                name = name,
+                url = finalUrl,
+                type = ExtractorLinkType.M3U8
+            ) {
+                this.referer = baseOf(finalUrl)
+                this.quality = Qualities.Unknown.value
+            }
+        )
+        return links
+    }
+
+    val body = resp.text
+
+    // Step 3: cari m3u8 di dalam HTML / JS
+    val m3u8Regex = Regex("""https?:\/\/[^\s"'<>]+\.m3u8[^\s"'<>]*""")
+    val m3u8 = m3u8Regex.find(body)?.value
+
+    if (m3u8 != null) {
+        links.add(
+            newExtractorLink(
+                source = name,
+                name = name,
+                url = m3u8,
+                type = ExtractorLinkType.M3U8
+            ) {
+                this.referer = baseOf(m3u8)
+                this.quality = Qualities.Unknown.value
+            }
+        )
+        return links
+    }
+
+    return null
+    }
+
 }
+
+private fun baseOf(u: String): String =
+    URI(u).let { "${it.scheme}://${it.host}/" }
