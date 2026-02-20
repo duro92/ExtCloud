8f3b3ecf29b6500444d2e2a992b031aaba743dfe
Pendztagram <114850487+duro92@users.noreply.github.com>
2026-02-20 08:40:14 +0700

    Update Extractors.kt

--- Melongmovie/src/main/kotlin/com/melongmovie/Extractors.kt
+++ Melongmovie/src/main/kotlin/com/melongmovie/Extractors.kt
@@ -10,7 +10,10 @@ import com.lagradost.cloudstream3.utils.M3u8Helper.Companion.generateM3u8
 import com.lagradost.cloudstream3.utils.ExtractorLinkType
 import com.lagradost.cloudstream3.utils.Qualities
 import com.lagradost.cloudstream3.extractors.VidStack
+import com.lagradost.cloudstream3.extractors.StreamWishExtractor
 import java.net.URI
+import com.lagradost.cloudstream3.utils.newExtractorLink
+import org.mozilla.javascript.Context
 
 
 
@@ -72,6 +75,17 @@ class Melongfilmstrp2p : VidStack() {
     override var requiresReferer = true
 }
 
+class Ukokoko : VidStack() {
+    override var name = "Ukokoko"
+    override var mainUrl = "https://ukokoko.4meplayer.com"
+    override var requiresReferer = true
+}
+
+class Hglink : StreamWishExtractor() {
+    override val name = "Hglink"
+    override val mainUrl = "https://hglink.to"
+}
+
 open class Dintezuvio : ExtractorApi() {
     override val name = "Earnvids"
     override val mainUrl = "https://dintezuvio.com"
@@ -122,4 +136,46 @@ open class Dintezuvio : ExtractorApi() {
 		}
 	}
 
+}
+
+open class StreamTape : ExtractorApi() {
+    override var name = "StreamTape"
+    override var mainUrl = "https://streamtape.com"
+    override val requiresReferer = false
+
+    override suspend fun getUrl(url: String, referer: String?): List<ExtractorLink>? {
+        with(app.get(url)) {
+            var result =
+                this.document.select("script").firstOrNull { it.html().contains("botlink').innerHTML") }
+                    ?.html()?.lines()?.firstOrNull{ it.contains("botlink').innerHTML") }?.let {
+                        val scriptContent =
+                            it.substringAfter(").innerHTML").replaceFirst("=", "var url =")
+                        val rhino = Context.enter()
+                        rhino.setInterpretedMode(true)
+                        val scope = rhino.initStandardObjects()
+                        var result = ""
+                        try {
+                            rhino.evaluateString(scope, scriptContent, "url", 1, null)
+                            result = scope.get("url", scope).toString()
+                        }finally {
+                            rhino.close()
+                        }
+                        result
+                    }
+            if(!result.isNullOrEmpty()){
+                val extractedUrl = "https:${result}&stream=1"
+                return listOf(
+                    newExtractorLink(
+                        name,
+                        name,
+                        extractedUrl,
+                    ) {
+                        this.referer = url
+                        this.quality = Qualities.Unknown.value
+                    }
+                )
+            }
+        }
+        return null
+    }
 }
