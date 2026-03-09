8df6461b73d99778ea1dfb641ec8f100ab863c67
duro92 <pendisu647@gmail.com>
2026-03-09 12:50:00 +0700

    Fix

---
+++ AnimeSailProvider/src/main/kotlin/com/hexated/Extractors.kt
@@ -0,0 +1,11 @@
+package com.hexated
+
+import com.lagradost.api.Log
+import com.lagradost.cloudstream3.app
+import com.lagradost.cloudstream3.extractors.MixDrop
+
+
+
+class MixDropBz : MixDrop() {
+    override var mainUrl = "https://m1xdrop.bz"
+}
